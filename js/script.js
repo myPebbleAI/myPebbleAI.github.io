505a021a40c25133c10227c90b0e3cd7f297536d
myPebbleAI <hey@mypebble.ai>
2025-03-09 11:35:11 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1106,12 +1106,7 @@
 	var mouseX = 0,
 		mouseY = 0;
 
-	TweenMax.to({}, 0.016, {
-		repeat: -1,
-		onRepeat: function () {
-			posX += (mouseX - posX) / 9;
-			posY += (mouseY - posY) / 9;
-
+	
 			TweenMax.set(follower, {
 				css: {
 					left: posX - 12,
