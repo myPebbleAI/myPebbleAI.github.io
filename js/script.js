e6f6a36efbea01c19c5ec582dd8f7aa0e0747046
myPebbleAI <hey@mypebble.ai>
2025-03-09 06:20:35 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1147,45 +1147,6 @@
 
 
 
-
-
-
-	// Style 4 Zoom out
-	let splitTitleLines = gsap.utils.toArray(".title-anim");
-	splitTitleLines.forEach(splitTextLine => {
-		const tl = gsap.timeline({
-			scrollTrigger: {
-				trigger: splitTextLine,
-				start: 'top 90%',
-				end: 'bottom 60%',
-				scrub: false,
-				markers: false,
-				toggleActions: 'play none none none'
-			}
-		});
-		const itemSplitted = new SplitText(splitTextLine, {
-			type: "words, lines"
-		});
-		gsap.set(splitTextLine, {
-			perspective: 400
-		});
-		itemSplitted.split({
-			type: "lines"
-		})
-		tl.from(itemSplitted.lines, {
-			duration: 1,
-			delay: 0.3,
-			opacity: 0,
-			rotationX: -80,
-			force3D: true,
-			transformOrigin: "top center -50",
-			stagger: 0.1
-		});
-	});
-
-
-
-
 	// Add Current Class Auto
 	function dynamicCurrentMenuClass(selector) {
 		let FileName = window.location.href.split("/").reverse()[0];
