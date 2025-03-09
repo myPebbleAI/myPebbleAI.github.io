4213512a9d3ad4d43a8201bd035e10c0a04ea4f8
myPebbleAI <hey@mypebble.ai>
2025-03-09 06:17:15 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1144,54 +1144,7 @@
 
 
 
-	//  Animation Fade Left End
-	/////////////////////////////////////////////////////
-	// CURSOR
-	var cursor = $(".cursor"),
-		follower = $(".cursor-follower");
 
-	var posX = 0,
-		posY = 0;
-
-	var mouseX = 0,
-		mouseY = 0;
-
-	TweenMax.to({}, 0.016, {
-		repeat: -1,
-		onRepeat: function () {
-			posX += (mouseX - posX) / 9;
-			posY += (mouseY - posY) / 9;
-
-			TweenMax.set(follower, {
-				css: {
-					left: posX - 12,
-					top: posY - 12
-				}
-			});
-
-			TweenMax.set(cursor, {
-				css: {
-					left: mouseX,
-					top: mouseY
-				}
-			});
-		}
-	});
-
-	$(document).on("mousemove", function (e) {
-		mouseX = e.clientX;
-		mouseY = e.clientY;
-	});
-	//circle
-	$(".theme-btn, a").on("mouseenter", function () {
-		cursor.addClass("active");
-		follower.addClass("active");
-	});
-	$(".theme-btn, a").on("mouseleave", function () {
-		cursor.removeClass("active");
-		follower.removeClass("active");
-	});
-	// CURSOR End
 
 
 
